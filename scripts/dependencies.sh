eed3f4dc5eefbca4a816b36a615cc23585f8ad02
Emma Doyle <emma.ann.doyle@gmail.com>
2026-01-19 00:28:39 -0500

    master: not sure

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -7,7 +7,7 @@ asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 
 # Install Ruby dependencies.
 # https://github.com/rbenv/ruby-build/wiki#ubuntudebianmint
-sudo apt-get install autoconf bison patch build-essential rustc libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libgmp-dev libncurses5-dev libffi-dev libgdbm6 libgdbm-dev libdb-dev uuid-dev
+# sudo apt-get install autoconf bison patch build-essential rustc libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libgmp-dev libncurses5-dev libffi-dev libgdbm6 libgdbm-dev libdb-dev uuid-dev
 
 asdf install
 
